6dcd8a6b7b42598e3db83e78816ce3a4ce2ab70f
Thomas Kadauke <tkadauke@imedo.de>
2013-06-12 11:48:04 +0200

    Task to deploy to all devices in a row.

--- lib/motion/project/devices.rb
+++ lib/motion/project/devices.rb
@@ -3,12 +3,19 @@ file_paths = ['config/devices.yml', "#{ENV['HOME']}/.devices.yml", '/etc/devices
 file_paths.each do |path|
   if File.exists?(path)
     namespace :device do
+      tasks = []
+      
       YAML.load(File.read(path)).each do |name, id|
+        tasks << name
+        
         desc "Deploy on #{name}"
         task name do
           sh "rake device id=#{id}"
         end
       end
+      
+      desc "Deploy on all devices"
+      task :all => tasks
     end
   end
 end
